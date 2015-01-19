6d67e22bf93890a4fe45104ae5a61d81cdee1230
Jiafeng Wu <jack.wu1224@gmail.com>
2015-01-18 18:28:47 -0700

    change add_claim layout

--- gen/com/example/assignment1/R.java
+++ gen/com/example/assignment1/R.java
@@ -29,32 +29,30 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Add_Button=0x7f09000b;
-        public static final int Cancel_Button=0x7f09000c;
-        public static final int Currency_spinner=0x7f090016;
-        public static final int Currency_textView=0x7f090015;
-        public static final int Description_editText=0x7f090012;
-        public static final int Description_textView=0x7f090011;
-        public static final int Descriptions_editText=0x7f09000a;
-        public static final int Descrption_textView=0x7f090009;
+        public static final int Add_Button=0x7f090006;
+        public static final int Cancel_Button=0x7f09000a;
+        public static final int Currency_spinner=0x7f090014;
+        public static final int Currency_textView=0x7f090013;
+        public static final int Description_editText=0x7f090010;
+        public static final int Description_textView=0x7f09000f;
+        public static final int Descriptions_editText=0x7f090005;
+        public static final int Descrption_textView=0x7f090007;
         public static final int Destination_editText=0x7f090008;
-        public static final int Destination_textView=0x7f090007;
+        public static final int Destination_textView=0x7f090001;
         public static final int Expense_View=0x7f090000;
-        public static final int Foe_textView=0x7f09000d;
+        public static final int Foe_textView=0x7f09000b;
         public static final int FromDate_textView=0x7f090004;
         public static final int From_editText=0x7f090002;
-        public static final int HowMuch_editText=0x7f090014;
-        public static final int HowMuch_textView=0x7f090013;
-        public static final int Name_editText=0x7f090005;
-        public static final int Name_textView=0x7f090001;
-        public static final int ToDate_textView=0x7f090006;
+        public static final int HowMuch_editText=0x7f090012;
+        public static final int HowMuch_textView=0x7f090011;
+        public static final int ToDate_textView=0x7f090009;
         public static final int To_editText=0x7f090003;
-        public static final int When_editText=0x7f09000f;
-        public static final int When_textView=0x7f09000e;
-        public static final int action_settings=0x7f090018;
-        public static final int addClaim=0x7f090019;
-        public static final int for_items_spinner=0x7f090010;
-        public static final int scrollView1=0x7f090017;
+        public static final int When_editText=0x7f09000d;
+        public static final int When_textView=0x7f09000c;
+        public static final int action_settings=0x7f090016;
+        public static final int addClaim=0x7f090017;
+        public static final int for_items_spinner=0x7f09000e;
+        public static final int scrollView1=0x7f090015;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
